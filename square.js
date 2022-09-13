0c122818de5e4a5603c0e467cb9399420e0412c2
aakarsh277 <109575548+aakarsh277@users.noreply.github.com>
2022-09-13 17:11:34 +0530

    Add files via upload

---
+++ square.js
@@ -0,0 +1,7 @@
+function square(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log("square of", i, "is", i * i)
+    }
+}
+
+square(8);
---
+++ sweets.js
@@ -0,0 +1,11 @@
+function sweets(n) {
+    if (n % 2 == 0) {
+        console.log("yes it can be distributed");
+    }
+    else {
+        console.log("no it cannot be distributed")
+    }
+}
+
+sweets(8);
+sweets(7);
